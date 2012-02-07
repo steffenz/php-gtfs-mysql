89eeb9c0efccf26fc1598b5a7cbd9587cd3ef206
Steffen Martinsen <steffen@steffenmedia.no>
2012-02-07 16:15:00 +0100

    Script is now working. Cleaning up and making guided version

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * mySQL Setup:
+ *
+ * You need to setup these properly. 
+ * The 'sqlHost' is typically localhost
+ *
+ */
+
+$sqlUser 	= "root";
+$sqlPass 	= "root";
+$sqlHost 	= "localhost:8889";
+$sqlTable 	= "my-awesome-table";
+$sqlCharset = "utf8";
+
+
+
+?>
